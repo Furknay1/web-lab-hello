a9cda12551ff92ded1e3f56c0942d88203ca51c2
Furkan Ay <furknay1@gmail.com>
2026-02-24 21:21:20 +0300

    chore: initial project setup with Vite React + TS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+ return (
+ <div >
+ <h1 >Web Tasarimi ve Programlama </h1 >
+ <h2 >LAB -1</h2 >
+ <p>Ad Soyad: [Furkan AY]</p>
+ <p>Ogrenci No: [230542026]</p>
+ </div >
+ )
+ }
+
+ export default App
