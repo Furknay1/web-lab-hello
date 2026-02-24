4f02ff28ce158c641a8af581ae68359d7f5e86ea
Furkan Ay <furknay1@gmail.com>
2026-02-24 21:25:28 +0300

    feat: add personal details to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App() {
  <h1 >Web Tasarimi ve Programlama </h1 >
  <h2 >LAB -1</h2 >
  <p>Ad Soyad: [Furkan AY]</p>
- <p>Ogrenci No: [230542026]</p>
+ <p>Ogrenci No: [230542026]</p>git add .
+ <p>hobi:[futbol]</p>
  </div >
  )
  }
